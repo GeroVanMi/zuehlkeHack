4a4ebd48a02dc4fd2949c3bd51b2f70363828640
Gerome Meyer <gerome.meyer@icloud.com>
2021-08-14 15:10:28 +0200

    Renamed page components

--- views/ActiveRound.js
+++ views/ActiveRound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Home() {
+export default function ActiveRound() {
     return (
         <View style={styles.container}>
             <Text>Round ongoing!</Text>
